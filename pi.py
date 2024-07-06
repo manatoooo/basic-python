fd4d28b3d4d81fcbc03f0cb5db6fa3412da1a018
manatoooo <manatoooo1234@gmail.com>
2024-07-06 21:50:43 +0900

    課題２

--- pi.py
+++ pi.py
@@ -4,4 +4,11 @@ text = """
 """
 
 # TODO
-
+def countwords(text):
+    words=text.split()
+    return ''.join(map(lambda word: str(len(word)),words))
+import re
+removesign=text.lower()
+removesign=re.sub(r'[^\w\s]','',removesign)
+result =countwords(removesign)
+print(result)
