5eded68a85eb8437db553f50a09478febe0d47d7
manatoooo <manatoooo1234@gmail.com>
2024-07-13 17:07:39 +0900

    課題３

--- euclid.py
+++ euclid.py
@@ -2,3 +2,19 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+x=int(a)
+y=int(b)
+if x > y:
+    while y != 0: 
+        z = x % y
+        x = y
+        y = z
+    print(str(x))
+elif x < y:
+    while x != 0: 
+        z = y % x
+        y = x
+        x = z
+    print(str(y))
+else:
+    print(str(x))
