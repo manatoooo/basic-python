463888de90fedc633b9524428697673cac8e74f6
manatoooo <manatoooo1234@gmail.com>
2024-07-13 17:07:53 +0900

    課題２

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,19 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+import math
+def f(x):
+    return sin(x)
+
+a = 0
+b = math.pi / 2
+n = 100
+
+h = (b - a) / n
+
+sum = 0.5 * (f(a) + f(b))
+for i in range(1, n):
+    sum += f(a + i * h)
+sum *= h
+
+print("答えは"+str(sum))
