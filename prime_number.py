9f77b685ba4e950adfa5fb0b44958cb81064d7e2
manatoooo <manatoooo1234@gmail.com>
2024-07-13 17:08:03 +0900

    課題１

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,23 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+x=int(a)      
+y=int(b)              
+def primenum(n):
+    if int(n) < 2:
+     return False
+
+    for i in range(2,n):
+        if n%i ==0:
+            return False
+    return True
+
+if primenum(x):
+   print(str(x)+"は素数")
+else:
+    print(str(x)+"は素数ではない")
+
+if primenum(y):
+   print(str(y)+"は素数")
+else:
+    print(str(y)+"は素数ではない")
